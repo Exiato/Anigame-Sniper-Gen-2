defb1dc35dc3496837644aece7d687c5f1269a74
Exiato <83570576+Exiato@users.noreply.github.com>
2021-08-19 10:28:13 +0530

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 title Anigame Sniper Modules Installer
-py -3.8 -m pip install datetime
-py -3.8 -m pip install colorama
-py -3.8 -m pip install discord.py
+pip install datetime
+pip install colorama
+pip install discord.py
 echo Finished!
 pause
